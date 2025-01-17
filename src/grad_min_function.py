a4c41c911f7d97940c53a8e0513249057ee0c743
Nearez <97766579+Nearezz@users.noreply.github.com>
2025-01-16 20:50:34 -0500

    Used my brain and made it better

--- src/grad_min_function.py
+++ src/grad_min_function.py
@@ -1,7 +1,8 @@
 ## Using Gradient Descent to minimize the function (x+5)^2
 import matplotlib.pyplot as plt
 import numpy as np
-
+import random
+import math
 
 ## Creating the graph of f(x)
 def f(x): 
@@ -13,15 +14,20 @@ def f_prime(x):
 x = np.linspace(-10,0,100)
 y = f(x)
 plt.plot(x,y,label="f(x) = x+5^2",color="blue")
+iterations = True
 
+x_old = random.uniform(-10,0)
+while (iterations): 
+    if abs(f_prime(x_old)) < math.pow(10,-13):
+        iterations = False
 
-lr = 0.1 ## The learning rate is how much the function will update.
-
-x_old = -4 ##stating from a random value of x technically can be anything 
-for i in range(10):
+    lr = random.uniform(0,1)
     x_new = x_old - (lr * f_prime(x_old)) ## important fourmula
     y_new = f(x_new)
 
     plt.plot(x_new,y_new,"ro")
+    print("The new x value is: ",x_new)
     x_old = x_new
-plt.show
+
+
+plt.show()
