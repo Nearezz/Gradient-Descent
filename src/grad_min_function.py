48d50e42f9437e6f14b8c8cee0183c6fc8942c04
Nearez <97766579+Nearezz@users.noreply.github.com>
2025-01-16 19:03:22 -0500

    made an adjumst with the learning rate

--- src/grad_min_function.py
+++ src/grad_min_function.py
@@ -1,2 +1,27 @@
-## Using Gradient Descent to Minimize A function 
+## Using Gradient Descent to minimize the function (x+5)^2
+import matplotlib.pyplot as plt
+import numpy as np
 
+
+## Creating the graph of f(x)
+def f(x): 
+    return (x+5)**2
+
+def f_prime(x):
+    return 2*(x+5)
+
+x = np.linspace(-10,0,100)
+y = f(x)
+plt.plot(x,y,label="f(x) = x+5^2",color="blue")
+
+
+lr = 0.1 ## The learning rate is how much the function will update.
+
+x_old = -4 ##stating from a random value of x technically can be anything 
+for i in range(10):
+    x_new = x_old - (lr * f_prime(x_old)) ## important fourmula
+    y_new = f(x_new)
+
+    plt.plot(x_new,y_new,"ro")
+    x_old = x_new
+plt.show
